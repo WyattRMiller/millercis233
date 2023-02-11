54ae6e0b30c601508767e6888c3374893116998c
Wyatt87 <wyzitdudeman@gmail.com>
2023-02-10 17:35:29 -0800

    seeded database, generated product controller

--- crud-app/database/seeders/ProductSeeder.php
+++ crud-app/database/seeders/ProductSeeder.php
@@ -14,6 +14,19 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Product::query()->delete();
+
+        $faker = \Faker\Factory::create();
+
+
+        foreach(range(1,30) as $number) {
+            \App\Models\Product::create([
+                'name' => 'Product',
+                'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 1000000.00),
+                'description' => 'This is the description of a product',
+                'item_number' => $faker->numberBetween($min = 0, $max = 10000),
+                'image' => $faker->imageUrl
+            ]);
+        }
     }
 }
--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', function () {
+    $products = App\Models\Product::all();
+    return view('products', ['products' => $products]);
 });
