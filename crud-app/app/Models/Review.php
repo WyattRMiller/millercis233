d485f418b691bd0fec34aa95174df960d7dfa075
Wyatt87 <wyzitdudeman@gmail.com>
2023-02-25 15:16:36 -0800

    Created table relationships

--- crud-app/app/Models/Review.php
+++ crud-app/app/Models/Review.php
@@ -10,4 +10,8 @@ class Review extends Model
     use HasFactory;
 
     protected $fillable = ['comment', 'rating', 'product_id'];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
