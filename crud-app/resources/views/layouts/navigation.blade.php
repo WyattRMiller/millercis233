ef9b922b50d862d534cf625a76b94b59ce936f20
Wyatt87 <wyzitdudeman@gmail.com>
2023-03-10 17:52:54 -0800

    Added products and users to navigation

--- crud-app/resources/views/layouts/navigation.blade.php
+++ crud-app/resources/views/layouts/navigation.blade.php
@@ -16,7 +16,19 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-            </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
+                        Products
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                        Users
+                    </x-nav-link>
+                </div>
+        </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
