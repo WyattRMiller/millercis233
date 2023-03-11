4587110d0345b83af8a06c797c9309b729b357a0
Wyatt87 <wyzitdudeman@gmail.com>
2023-03-10 17:00:18 -0800

    added auth to products and users routes

--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -21,11 +21,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
 
 Route::resource('reviews', ReviewController::class);
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
