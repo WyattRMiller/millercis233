4b66beb931d8aecf90dceec69f54ddad799f9f19
Wyatt87 <wyzitdudeman@gmail.com>
2023-03-15 17:21:30 -0700

    Re-seeded databases for the third time to fix product names

--- crud-app/database/seeders/ProductSeeder.php
+++ crud-app/database/seeders/ProductSeeder.php
@@ -23,7 +23,7 @@ class ProductSeeder extends Seeder
 
         foreach(range(1,100) as $number) {
             Product::create([
-                'name' => $faker->name,
+                'name' => $faker->word,
                 'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100.00),
                 'description' => 'This is the description of a product',
                 'item_number' => $faker->numberBetween($min = 0, $max = 10000),
