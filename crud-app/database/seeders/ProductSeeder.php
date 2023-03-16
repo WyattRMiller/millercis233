e52f15795e8d55e2debf33198e3a1deb0c9b0cc1
Wyatt87 <wyzitdudeman@gmail.com>
2023-03-15 17:13:51 -0700

    Re-seeded databases

--- crud-app/database/seeders/ProductSeeder.php
+++ crud-app/database/seeders/ProductSeeder.php
@@ -21,13 +21,14 @@ class ProductSeeder extends Seeder
         $faker = \Faker\Factory::create();
 
 
-        foreach(range(1,30) as $number) {
+        foreach(range(1,100) as $number) {
             Product::create([
                 'name' => 'Product',
                 'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 1000000.00),
                 'description' => 'This is the description of a product',
                 'item_number' => $faker->numberBetween($min = 0, $max = 10000),
-                'image' => $faker->imageUrl
+                'image' => $faker->imageUrl,
+                'rating' => $faker->numberBetween($min = 1, $max = 5)
             ]);
         }
     }
