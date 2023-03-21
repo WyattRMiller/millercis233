74187cc6193e323e309587195c64248fdc4cad62
Wyatt87 <wyzitdudeman@gmail.com>
2023-03-20 17:41:54 -0700

    re-seeded databases to include more records to better showcase livewire functionality

--- crud-app/database/seeders/ProductSeeder.php
+++ crud-app/database/seeders/ProductSeeder.php
@@ -21,14 +21,13 @@ class ProductSeeder extends Seeder
         $faker = \Faker\Factory::create();
 
 
-        foreach(range(1,100) as $number) {
+        foreach(range(1,300) as $number) {
             Product::create([
                 'name' => $faker->word,
                 'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100.00),
                 'description' => 'This is the description of a product',
                 'item_number' => $faker->numberBetween($min = 0, $max = 10000),
                 'image' => $faker->imageUrl,
-                'rating' => $faker->numberBetween($min = 1, $max = 5)
             ]);
         }
     }
